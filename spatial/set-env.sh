8577f78dfaf5fe3d26327099f224b9052fe4e240
adamtimm <amtimm@gmail.com>
2020-06-17 15:38:24 -0500

    re-order docker pull commands

--- spatial/set-env.sh
+++ spatial/set-env.sh
@@ -3,6 +3,9 @@
 #runs in foreground
 
 echo 'please wait while we prep the environment (should take about 10 seconds)'
+docker pull timmam/pg_tileserv:Katacoda
+docker pull timmam/pg_featureserv:Katacoda
+
 echo 'starting the database'
 docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=nyc --name=pgsql crunchydata/crunchy-postgres-appdev
 
